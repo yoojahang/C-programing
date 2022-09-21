cd7fc5296e32e12c435f7afa555d293ff1290116
yoojahang <113015286+yoojahang@users.noreply.github.com>
2022-09-21 18:40:11 +0900

    Add files via upload

---
+++ datatype@print/print연습.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+	printf("정수 %d과 %d",153,-153);
+	printf("\n실수 %f와%f",42.195,-42.195);
+	printf("\n문자 %c와 %c",'A','a');
+	printf("\n문자열 %s와 %s","artificial lntelligence","마빈 민스키");
+	printf("\n자연수 %d,%o,%x",255,255,255);
+	return 0;
+}
+
